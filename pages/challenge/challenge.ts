e291430e9031ade119bd7ef7ea76962f74b33192
nzbin <nzb329@163.com>
2019-04-20 23:42:25 +0800

    挑战页面完善

--- pages/challenge/challenge.ts
+++ pages/challenge/challenge.ts
@@ -2,11 +2,15 @@ import { genRandNum } from '../../utils/util';
 
 Page({
   data: {
-    num: 0
+    num: 0,
+    percent: 100
   },
   onLoad() {
     this.setData!({
       num: genRandNum(10)
     })
   },
+  start() {
+
+  }
 })
