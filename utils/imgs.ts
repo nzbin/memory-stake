8aa3f76c0af216aff2029fe1c9ac8f6312e800b2
nzbin <nzb329@163.com>
2019-04-21 17:28:44 +0800

    修改图片域名

--- utils/imgs.ts
+++ utils/imgs.ts
@@ -1,5 +1,5 @@
 import { genNumStr } from './util';
 
 export function genImgUrl(num: number): string {
-  return `http://pov0j3r4v.bkt.clouddn.com/${genNumStr(num)}.jpg`
+  return `http://pqayn9kab.bkt.clouddn.com/${genNumStr(num)}.jpg`
 }
