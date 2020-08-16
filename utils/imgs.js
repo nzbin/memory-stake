4b683d864bc8a302be5685d475b33571a92ea6cd
nzbin <nzb329@163.com>
2020-08-16 22:42:15 +0800

    修改图片路径
    修改图片路径

--- utils/imgs.js
+++ utils/imgs.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var util_1 = require("./util");
 function genImgUrl(num) {
-    return "http://pqayn9kab.bkt.clouddn.com/" + util_1.genNumStr(num) + ".jpg";
+    return "https://memory-stake.oss-cn-beijing.aliyuncs.com/" + util_1.genNumStr(num) + ".jpg";
 }
 exports.genImgUrl = genImgUrl;
 //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW1ncy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImltZ3MudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSwrQkFBbUM7QUFFbkMsU0FBZ0IsU0FBUyxDQUFDLEdBQVc7SUFDbkMsT0FBTyxzQ0FBb0MsZ0JBQVMsQ0FBQyxHQUFHLENBQUMsU0FBTSxDQUFBO0FBQ2pFLENBQUM7QUFGRCw4QkFFQyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IGdlbk51bVN0ciB9IGZyb20gJy4vdXRpbCc7XG5cbmV4cG9ydCBmdW5jdGlvbiBnZW5JbWdVcmwobnVtOiBudW1iZXIpOiBzdHJpbmcge1xuICByZXR1cm4gYGh0dHA6Ly9wcWF5bjlrYWIuYmt0LmNsb3VkZG4uY29tLyR7Z2VuTnVtU3RyKG51bSl9LmpwZ2Bcbn0iXX0=
